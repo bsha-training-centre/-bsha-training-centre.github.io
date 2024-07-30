2962ba03767e8aaa1349deff0a4adccc18cdddf1
wagenrace <nijhof.dns@gmail.com>
2024-07-30 20:42:59 +0200

    add basic background

--- src/views/Certificate.vue
+++ src/views/Certificate.vue
@@ -5,17 +5,31 @@ import { useRoute, useRouter } from "vue-router"
 const router = useRouter()
 const route = useRoute()
 
-const search = computed({
+const userId = computed({
   get() {
-    return route.query.search ?? ""
-  },
-  set(search) {
-    router.replace({ query: { search } })
+    return route.query.userId ?? ""
   }
 })
+
+const verificationId = computed({
+  get() {
+    return route.query.verificationId ?? ""
+  }
+})
+
 </script>
 
 <template>
-  <h2>AboutView</h2>
-  <label> Search: <input v-model.trim="search" maxlength="20" /> </label>
+  <div class="h-screen w-screen bg-white flex">
+    <div class="h-full w-full flex justify-center">
+        <img class="absolute top-0 left-0 w-full" src="@/assets/certificate-template.svg" alt="Certificate Template"
+        author="insspirito" original_url="pixabay.com/vectors/certificate-diploma-background-1237073/"/>
+        <div class="h-full flex flex-col justify-center bg-slate-400">
+          <h2>AboutView</h2>
+          <p> userId: {{ userId }} </p>
+          <p> verificationId: {{ verificationId }} </p>
+        </div>
+      </div>
+
+    </div>
 </template>
