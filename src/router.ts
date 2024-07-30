ca55a5e34f2ffd69e58d48add0f2c543e230a61f
Tom Nijhof <nijhof.dns@gmail.com>
2024-07-30 11:53:07 +0200

    Bugfix/router url (#4)
    * refactor: Update main.ts and router.js for improved code structure and performance
    
    * change router to ts
    
    * format

---
+++ src/router.ts
@@ -0,0 +1,22 @@
+import { createWebHistory, createRouter } from "vue-router"
+import { Router } from "vue-router"
+
+const routes = [
+  {
+    path: import.meta.env.BASE_URL,
+    name: "Home",
+    component: () => import("./views/Home.vue")
+  },
+  {
+    path: import.meta.env.BASE_URL + "certificate",
+    name: "Certificate",
+    component: () => import("./views/Certificate.vue")
+  }
+]
+
+const router: Router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
